eb5ef23012677b1348c476b2c361438bb0519ae5
Anikait <anikait.kapoor@tmotions.com>
2026-06-16 09:55:44 +0530

    file structure changes

--- src/tm_logger/__init__.py
+++
@@ -1,3 +0,0 @@
-from .logger import DEBUG, LOG_DIR, logger, logger_title, setup_logger
-
-__all__ = ["DEBUG", "LOG_DIR", "logger", "logger_title", "setup_logger"]
---
+++ src/tm_utility/logger/__init__.py
@@ -0,0 +1,3 @@
+from .logs import DEBUG, LOG_DIR, logger, logger_title, setup_logger
+
+__all__ = ["DEBUG", "LOG_DIR", "logs", "logger_title", "setup_logger"]
